daa444ba4edc0956ecb6a04c55942bcd51c03023
tiyee <tiyee@live.com>
2019-05-04 16:48:35 +0800

    s

--- src/angemon/event.h
+++ src/angemon/event.h
@@ -5,23 +5,23 @@
 using angemon::Context;
 namespace angemon {
 namespace event {
-enum STATE{
-    E_READ  =		0x1,
-   E_WRITE 	=	0x2,
-    E_SIGNAL		=0x4,
-    E_TIMEOUT		=0x8,
- E_EDGE 		=	0x10,/* edge triggered */
- E_ONCE		=	0x20,/* one-time event */
- E_IN_REACTOR 	=0x40,/* indicates whether the event is in the reactor */
+
+    const int  E_READ  =		0x1;
+   const int E_WRITE 	=	0x2;
+   const int  E_SIGNAL		=0x4;
+   const int  E_TIMEOUT		=0x8;
+ const int E_EDGE 		=	0x10;/* edge triggered */
+ const int E_ONCE		=	0x20;/* one-time event */
+ const int E_IN_REACTOR 	=0x40/* indicates whether the event is in the reactor */
 /*
 * The timer event's initial timerheap_idx value,
 * indicates that the event is not in the heap.
 */
-    E_OUT_OF_TIMERHEAP =0,
+   const int  E_OUT_OF_TIMERHEAP =0;
 };
 struct Event{
     const int fd;
-    short ev_flags;
+    int  ev_flags;
     Context* ctx;
     void * data;
     std::function<int(Context*)> cb ;
