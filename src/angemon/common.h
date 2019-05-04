b485a312a12100304ab0467abe96e6fd59344abc
tiyee <tiyee@live.com>
2019-05-04 15:44:27 +0800

    test linux

--- src/angemon/common.h
+++ src/angemon/common.h
@@ -1,9 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <cerrno>
-#include <cstddef>
-#include <ctime>
+#include <errno.h>
+#include <stddef.h>
+#include <time.h>
 #include <iostream>
 #include <list>
 #include <map>
