c53625ca369e7a600382daada754a2010b648bbd
tiyee <tiyee@live.com>
2019-08-13 21:54:24 +0800

    add socket

--- src/socket.h
+++ src/socket.h
@@ -1,9 +1,14 @@
 #ifndef SOCKET_H
 #define SOCKET_H
+#include "event.h"
 namespace angemon {
 class Socket {
  public:
   Socket();
+  static void accept(int fd);
+  static void write(const Event*& ev);
+  static void read(const Event*& ev);
+  static void close(const Event*& ev);
 };
 
 }  // namespace angemon
